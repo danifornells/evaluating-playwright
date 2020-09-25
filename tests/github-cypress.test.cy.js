c88fef59e0fe7420da2bc63129b1f17281229cc0
Daniel Fornells <daniel.garcia.fornells@gmail.com>
2020-09-25 10:29:39 +0200

    Add Cypress version

---
+++ tests/github-cypress.test.cy.js
@@ -0,0 +1,26 @@
+/**
+ * Using Cypress with testing-library
+ *
+ * CYPRESS ...................... https://docs.cypress.io/api
+ * CYPRESS-TESTING-LIBRARY ...... https://github.com/testing-library/cypress-testing-library
+ *
+ * **/
+
+import '@testing-library/cypress/add-commands'
+
+context('GitHub', () => {
+  it('should show the microsoft/Playwright project in the search if you search for Playwright', () => {
+    cy.visit('https://github.com')
+    cy.findByPlaceholderText( 'search',{ exact: false })
+      .type('Playwright{enter}')
+    cy.findByRole("link", { name: "microsoft/playwright" })
+      .should('exist')
+  })
+
+  it('should contain \'Playwright\' in the project title', () => {
+    cy.findByRole("link", { name: "microsoft/playwright" })
+      .click()
+    cy.get("#readme h1")
+      .should('contain.text', 'Playwright')
+  })
+})
